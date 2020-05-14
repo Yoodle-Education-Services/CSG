08e64bd8583ad4b0d856a36b442980d827d826dc
fekadugurmu <60789622+fekadugurmu@users.noreply.github.com>
2020-05-14 10:48:26 -0500

    framework component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrameworkComponent } from './framework/framework.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrameworkComponent
   ],
   imports: [
     BrowserModule,
